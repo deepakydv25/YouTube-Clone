414cc43d24b7d2563f92fa910ffdc1602dffba60
Deepak Yadav <deepaksyadav00@gmail.com>
2023-02-11 22:50:56 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,3 @@
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=es&videoCategoryId=17&key=" +
+  process.env.API_KEY;
